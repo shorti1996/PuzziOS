b036c3fdb1cfefb81024115671754b46f004566f
shorti1996 <shorti1996@gmail.com>
2018-10-31 13:38:15 +0100

    Initial Commit

---
+++ Puzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Puzz
+//
+//  Created by Wojciech Liebert on 31/10/2018.
+//  Copyright © 2018 Wojciech Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
